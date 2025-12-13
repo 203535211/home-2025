cd27bfbd376baf4218acdda38b82b0098b2d3f04
203535211 <34734036+203535211@users.noreply.github.com>
2025-12-13 14:47:41 +0800

    Add files via upload

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, DeviceTv, BattleNet, Backhoe, BrightnessAutoFilled, Bitcoin, Circles } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, BattleNet, Bitcoin } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -70,12 +70,8 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
-  DeviceTv,
   BattleNet,
-  Backhoe,
-  BrightnessAutoFilled,
   Bitcoin,
-  Circles,
 };
 
 // 链接跳转
